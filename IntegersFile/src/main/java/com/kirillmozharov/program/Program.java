98f384bc456a468fc073ef8b94431a062cfbd4f5
mozharovkv <mozharovkv@mozharov-mbp2.local>
2023-03-02 20:32:04 +0300

    integers repository

---
+++ IntegersFile/src/main/java/com/kirillmozharov/program/Program.java
@@ -0,0 +1,29 @@
+package com.kirillmozharov.program;
+
+import com.kirillmozharov.repository.IntegersRepository;
+
+import java.io.IOException;
+
+/**
+ * @author mozharovkv
+ */
+public class Program {
+    public static void main(String[] args) {
+        try {
+            IntegersRepository repository = new IntegersRepository("input.txt");
+            System.out.println(repository);
+            repository.add(777);
+            repository.add(797);
+            repository.add(77);
+            repository.add(7);
+            repository.deleteSameDigitsNumbers();
+            try {
+                repository.save("out.txt");
+            } catch (IOException e){}
+
+            System.out.println(repository);
+        } catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
---
+++ IntegersFile/src/main/java/com/kirillmozharov/repository/IntegersRepository.java
@@ -0,0 +1,111 @@
+package com.kirillmozharov.repository;
+
+import com.kirillmozharov.util.Util;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class IntegersRepository {
+
+
+    private ArrayList<Integer> nums = new ArrayList<Integer>();
+
+    public IntegersRepository() {
+    }
+
+    /**
+     * 8.	Модифицировать конструктор IntegersRepository таким образом,
+     * чтобы он загружал все числа, которые есть в файле,
+     * при этом теперь числа могут следовать как через пробел, так и с новой строки
+     * @param filename
+     * @throws IOException
+     */
+    public IntegersRepository(String filename) throws IOException {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(filename))) {
+            while (bufferedReader.ready()) {
+                String line = bufferedReader.readLine();
+                String[] splitted = line.split(" ");
+                for (String s : splitted) {
+                    this.nums.add(Integer.parseInt(s));
+                }
+            }
+        }
+    }
+
+    public void add(Integer value) {
+        this.nums.add(value);
+    }
+
+    public void save(String filename) throws IOException {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(filename))) {
+            for (int i = 0; i < this.nums.size(); i++) {
+                bufferedWriter.write(this.nums.get(i).toString());
+                bufferedWriter.newLine();
+            }
+        }
+    }
+
+
+    /**
+     *
+     * @return
+     */
+    public int countMaxInARow() {
+        return maxInARow(this.nums);
+    }
+
+    /**
+     * 6. Найти максимальное количество подряд идущих чисел в списке чисел
+     * @param list
+     * @return
+     */
+    private int maxInARow(ArrayList<Integer> list) {
+        int maxInARow = 1;
+        for (int i = 0; i < list.size() - 1; i++) {
+            if (Objects.equals(list.get(i), list.get(i + 1))) {
+                maxInARow += 1;
+            } else {
+                maxInARow = 1;
+            }
+        }
+        return maxInARow;
+    }
+
+    /**
+     * 7. Удалить из исходного списка все числа с одинаковыми цифрами (например, такие как 111, 11, 22, 33)
+     */
+    public void deleteSameDigitsNumbers() {
+        ArrayList<Integer> toDelete = new ArrayList<>();
+        for (Integer num : this.nums) {
+            ArrayList<Integer> digitList = Util.numberToDigitList(num);
+            if (maxInARow(digitList) == digitList.size()) {
+                toDelete.add(num);
+            }
+        }
+        for (Integer integer : toDelete) {
+            this.nums.remove(integer);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "IntegersRepository{" +
+                "nums=" + nums +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+       IntegersRepository that = (IntegersRepository) o;
+        return Objects.equals(nums, that.nums);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nums);
+    }
+}
+
