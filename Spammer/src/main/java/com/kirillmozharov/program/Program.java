f5f4d115ea9ea1766a0c66e9e4865a41f7dad4cd
Kirill Mozharov <mozharovkv@m2.ru>
2023-03-16 14:28:23 +0300

    Spammer

---
+++ Spammer/src/main/java/com/kirillmozharov/program/Program.java
@@ -0,0 +1,24 @@
+package com.kirillmozharov.program;
+
+import java.util.*;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        String[] teams = new String[num];
+        HashSet<String> spammersSet = new HashSet<>();
+        for (int i = 0; i < num; i++) {
+            teams[i] = scanner.next();
+        }
+        HashSet<String> teamSet = new HashSet<>();
+        for (String team : teams) {
+            if (teamSet.contains(team)) {
+                spammersSet.add(team);
+            } else {
+                teamSet.add(team);
+            }
+        }
+        System.out.println(spammersSet.toString());
+    }
+}
