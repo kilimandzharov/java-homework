49d5f199e32ad5ad1a8ef3ead38e09bb5e543017
Kirill Mozharov <mozharovkv@m2.ru>
2023-03-16 19:15:46 +0300

    hi

---
+++ SessionHelper/src/main/java/com/kirillmozharov/program/Program.java
@@ -0,0 +1,38 @@
+package com.kirillmozharov.program;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Program {
+    /**
+     * Исходные данные
+     * В первой строке содержится число N — количество записей в списке преподавателя.
+     * Затем идет N строк, содержащих список преподавателя, по одной дате в строке.
+     * Записаны только года. Каждый год — целое число. Даты в этом списке отсортированы по неубыванию. В следующей после списка строке содержится число M — количество записей в списке студента. Затем также M строк с датами (записаны только года, каждый год — целое число). Этот список не отсортирован. В списке как студента, так и преподавателя даты могут повторяться.
+     * Результат
+     * Вы должны вывести одно число — количество чисел во втором списке, которые также содержатся в первом.
+     * @param args
+     */
+    public static void main(String[] args) {
+        int result = 0;
+        Scanner scanner = new Scanner(System.in);
+        int teacherLength = scanner.nextInt();
+        ArrayList<Integer> teacherList = new ArrayList<>();
+        for (int i = 0; i < teacherLength; i++) {
+            teacherList.add(scanner.nextInt());
+        }
+
+        int studentLength = scanner.nextInt();
+        ArrayList<Integer> studentList = new ArrayList<>();
+        for (int i = 0; i < studentLength; i++) {
+            studentList.add(scanner.nextInt());
+        }
+
+        for (Integer item : studentList) {
+            if(teacherList.contains(item)){
+               result++;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
