4505fe559f516c3aec57f0dc462a4b436080ed10
Kirill Mozharov <mozharovkv@m2.ru>
2023-03-16 13:53:15 +0300

    line arguments

--- CommandLinesArguments/src/main/java/com/kirillmozharov/program/Program.java
+++ CommandLinesArguments/src/main/java/com/kirillmozharov/program/Program.java
@@ -1,10 +1,12 @@
 package com.kirillmozharov.program;
 
+import java.util.Arrays;
+
 /**
  * 2.	Программа принимает на вход число n в виде аргумента командной строки из массива args. Если число n:
- *  -- Равно 1, то программа должна принять из аргументов командной строки число t и вывести на экран
+ * -- Равно 1, то программа должна принять из аргументов командной строки число t и вывести на экран
  * таблицу размером t на t, заполненную числами по порядку от единицы и тд
- *  -- Равно 2, то программа должна принять из аргументов командной строки число t и число k и вывести
+ * -- Равно 2, то программа должна принять из аргументов командной строки число t и число k и вывести
  * на экран таблицу размером t на k, заполненную числами по порядку от единицы и тд c конца таблицы
  * Пример:
  * Ввод
@@ -16,25 +18,34 @@ package com.kirillmozharov.program;
  */
 public class Program {
     public static void main(String[] args) {
-        int option = Integer.parseInt(args[0]);
-        int[] matrixSize = {Integer.parseInt(args[1]), Integer.parseInt(args[2])};
-        int iterator = option == 1 ? 1 : -1;
-        int count = option == 1 ? 1: matrixSize[0] * matrixSize[1];
-        int[][] result = new int[matrixSize[0]][matrixSize[1]];
-        for (int[] ints : result) {
-            for (int i = 0; i < ints.length; i++) {
-                ints[i] = count;
-                count += iterator;
+        if (args.length > 1) {
+            int k;
+            int rowLength = 0;
+            int colLength = 0;
+            int n = Integer.parseInt(args[0]);
+            int t = Integer.parseInt(args[1]);
+            if (n == 2) {
+                if (args.length > 2) {
+                    k = Integer.parseInt(args[2]);
+                    rowLength = t;
+                    colLength = k;
+                }
+            } else {
+                rowLength = t;
+                colLength = t;
             }
-        }
-        StringBuilder resultBuilder = new StringBuilder();
-        for (int[] ints : result) {
-            for (int anInt : ints) {
-                resultBuilder.append(anInt + " ");
+            int iterator = n == 2 ? -1 : 1;
+            int counter = n == 2 ? rowLength * colLength : 1;
+            int[][] result  = new int[rowLength][colLength];
+            for (int i = 0; i < result.length; i++) {
+                for (int i1 = 0; i1 < result[i].length; i1++) {
+                    result[i][i1] = counter;
+                    counter+=iterator;
+                }
             }
-            resultBuilder.append("\n");
+            System.out.println(Arrays.deepToString(result));
         }
-        System.out.println(resultBuilder);
-
     }
+
+
 }
